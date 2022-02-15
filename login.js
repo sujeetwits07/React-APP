89fad538ff032757fead3f7a716dbcff437074bf
sujeetwits07 <97097176+sujeetwits07@users.noreply.github.com>
2022-02-15 19:04:52 +0530

    Add files via upload

---
+++ login.js
@@ -0,0 +1,43 @@
+import React from "react";
+import { Body, Section, Para, Containt, But } from "./styled/css";
+const Login = () => {
+  return (
+    <>
+      <Body>
+        <Section>
+          <form action="">
+            <Para>
+              <h1>WIL</h1>
+            </Para>
+            <Containt>
+              <div>
+                <label htmlFor="email">Email</label>
+                <input
+                  type="text"
+                  name="email"
+                  id="email"
+                  placeholder="Enter your Email"
+                />
+              </div>
+              <div>
+                <label htmlFor="password">Password</label>
+                <input
+                  type="text"
+                  name="password"
+                  id="password"
+                  placeholder="Enter Your Password"
+                />
+              </div>
+            </Containt>
+            <But>
+              <button type="reset">Reset</button>
+              <button type="submit">Login</button>
+            </But>
+          </form>
+        </Section>
+      </Body>
+    </>
+  );
+};
+
+export default Login;
