f300242d4833b2733b6b10cfd45d91e6730202db
sujeetwits07 <97097176+sujeetwits07@users.noreply.github.com>
2022-02-15 19:01:21 +0530

    Add files via upload

--- login.js
+++ login.js
@@ -1,43 +1,60 @@
-import React from "react";
-import { Body, Section, Para, Containt, But } from "./styled/login";
-const Login = () => {
-  return (
-    <>
-      <Body>
-        <Section>
-          <form action="">
-            <Para>
-              <h1>WIL</h1>
-            </Para>
-            <Containt>
-              <div>
-                <label htmlFor="email">Email</label>
-                <input
-                  type="text"
-                  name="email"
-                  id="email"
-                  placeholder="Enter your Email"
-                />
-              </div>
-              <div>
-                <label htmlFor="password">Password</label>
-                <input
-                  type="text"
-                  name="password"
-                  id="password"
-                  placeholder="Enter Your Password"
-                />
-              </div>
-            </Containt>
-            <But>
-              <button type="reset">Reset</button>
-              <button type="submit">Login</button>
-            </But>
-          </form>
-        </Section>
-      </Body>
-    </>
-  );
-};
+import styled from "styled-components";
 
-export default Login;
+const Body = styled.div`
+  background-color: aliceblue;
+  display: flex;
+  height: 100vh;
+  width: 100%;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Section = styled.div`
+  font-family: Fantasy
+  display: flex;
+  justify-content: center;
+  width: 300px;
+  height: 500px;
+  border: 2px solid lightcoral;
+  border-radius: 30px;
+  background-image: url(https://wallpaperaccess.com/download/blur-phone-3967178);
+`;
+
+const Para = styled.div`
+  color: deepskyblue;
+  text-shadow: 1px 1px 2px black, 0 0 25px green, 0 0 5px lightgreen;
+  padding-top: 1px;
+  margin: 20px;
+  border: 1px solid darkred;
+  border-radius: 10px;
+  background-image: url(https://wallpaperaccess.com/download/blur-phone-5246918);
+`;
+
+const Containt = styled.div`
+  display: flex;
+  font-family: "Outfit", sans-serif;
+  flex-direction: column;
+  width: 100%;
+  height: 50%;
+  margin-top: 80px;
+  justify-content: center;
+  align-items: center;
+  gap: 30px;
+  div {
+    display: flex;
+    align-items: center;
+    font-family: "Outfit", sans-serif;
+  }
+`;
+
+const But = styled.div`
+  display: flex;
+  font-family: "Outfit", sans-serif;
+  width: 100%;
+  height: 50%;
+  margin-top: 20px;
+  border-radius: 10px;
+  padding-left: 163px;
+`;
+
+export { Body, Section, Para, Containt, But };
