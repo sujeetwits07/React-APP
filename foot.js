1ca928c6741f5a776e4bd0da71fd204402a3659c
sujeetwits07 <97097176+sujeetwits07@users.noreply.github.com>
2022-02-16 19:36:07 +0530

    Add files via upload

---
+++ foot.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Foot, Footlist } from "../../styled/css";
+const Footer = () => {
+  return (
+    <>
+      <Foot>
+        <Footlist>
+          <ul>
+            <li>Services</li>
+            <li>Social</li>
+            <li>Gallery</li>
+            <li>Settings</li>
+          </ul>
+        </Footlist>
+      </Foot>
+    </>
+  );
+};
+
+export default Footer;
