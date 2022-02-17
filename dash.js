fa88ba5491e9e140a062a20b1e5e141c41f8ae92
sujeetwits07 <97097176+sujeetwits07@users.noreply.github.com>
2022-02-17 19:12:52 +0530

    Add files via upload

---
+++ dash.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Main, Container } from "../../styled/css";
+const Dashboard = () => {
+  return (
+    <>
+      <Main>
+        <Container>
+          <h1>Wits Innovation Lab</h1>
+          <p>Hello, This is DashBoard</p>
+        </Container>
+
+      </Main>
+    </>
+  )
+
+};
+
+export default Dashboard;
