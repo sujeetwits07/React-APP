4ccb56b201a0784835c15465164578fa1c41a4ae
sujeetwits07 <97097176+sujeetwits07@users.noreply.github.com>
2022-02-17 19:12:38 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Navbar from "../navbar/nav";
+import Dashboard from "../main/dash";
+import Footer from "../footer/foot";
+
+function Main() {
+  return (
+    <>
+      <Navbar />
+        <Dashboard />
+      <Footer />
+    </>
+  );
+}
+export default Main;
